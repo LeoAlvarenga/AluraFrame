f4a502947f485acb3348ece3e089db7f2a15aef6
Leonardo Alvarenga <leonardoalvarenga@Leonardos-iMac-Pro.local>
2020-03-30 19:45:44 -0300

    Encapsulando Negociacao

--- js/app/models/Negociacoes.js
+++ js/app/models/Negociacoes.js
@@ -1,12 +1,25 @@
 class Negociacao {
 
     constructor(data, quantidade, valor) {
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        this._data = new Data(data.getTime());
+        this._quantidade = quantidade;
+        this._valor = valor;
+        Object.freeze(this);
     }
 
-    obtemVolume() {
-        return this.quantidade * this.valor;
+    get volume() {
+        return this._quantidade * this._valor;
+    }
+
+    get data() {
+        return new Date(this._data.getTime());
+    }
+
+    get quantidade() {
+        return this._quantidade;
+    }
+    
+    get valor() {
+        return this._valor;
     }
 }

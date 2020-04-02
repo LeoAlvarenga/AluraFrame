23405f9230a91b01f6cd1026ee51897f7c252978
Leonardo Alvarenga <leonardoalvarenga@Leonardos-iMac-Pro.local>
2020-04-02 15:03:11 -0300

    Implementando Padrão Factory

--- js/app/controllers/NegociacoesController.js
+++ js/app/controllers/NegociacoesController.js
@@ -8,15 +8,13 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-
-        this._listaNegociacoes = new ListaNegociacoes(model =>
-            this._negociacoesView.update(model));
-
+        
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update(this._listaNegociacoes);
-        this._mensagem = new Mensagem();
+        this._listaNegociacoes = new Bind (new ListaNegociacoes(), this._negociacoesView, 'adiciona', 'esvazia');
+
         this._mensagemView = new MensagemView($('#mensagemView'));
-        this._mensagemView.update(this._mensagem);
+        this._mensagem = new Bind(new Mensagem(), this._mensagemView, 'texto');
+
     }
 
     adiciona(event) {
@@ -25,7 +23,6 @@ class NegociacaoController {
         this._listaNegociacoes.adiciona(new Negociacao(DateHelper.textoParaData(this._inputData.value), this._inputQuantidade.value, this._inputValor.value));
 
         this._mensagem.texto= "Negociação adicionada com sucesso";
-        this._mensagemView.update(this._mensagem);
 
         this._limpaFormulario();
         
@@ -35,7 +32,6 @@ class NegociacaoController {
         this._listaNegociacoes.esvazia();
 
         this._mensagem.texto = 'Neogociações apagadas com sucesso';
-        this._mensagemView.update(this._mensagem);
     }
 
     _limpaFormulario() {

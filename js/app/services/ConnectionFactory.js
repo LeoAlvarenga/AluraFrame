be689a5e5838d64e5f0cf8d4831085e7e01d84be
Leonardo Alvarenga <leo.alvarenga08@hotmail.com>
2020-04-03 16:57:43 -0300

    Aplicando Module Pattern na ConnectionFactory

--- js/app/services/ConnectionFactory.js
+++ js/app/services/ConnectionFactory.js
@@ -1,49 +1,54 @@
-var stores = ['negociacoes'];
-var version = 3;
-var dbName = 'aluraframe';
+var ConnectionFactory = (function() {
 
-var connection = null;
+    var stores = ['negociacoes'];
+    var version = 3;
+    var dbName = 'aluraframe';
 
-class ConnectionFactory {
+    var connection = null;
 
-    constructor() {
+    return class ConnectionFactory {
 
-        throw new Error('Não é possível criar instâncias de ConnectionFactory');
+        constructor() {
+
+            throw new Error('Não é possível criar instâncias de ConnectionFactory');
+        }
+
+        static getConnection() {
+
+            return new Promise((resolve, reject) => {
+
+                let openRequest = window.indexedDB.open(dbName, version);
+
+                openRequest.onupgradeneeded = e => {
+
+                    ConnectionFactory._createStores(e.target.result);
+                };
+
+                openRequest.onsuccess = e => {
+
+                    if(!connection) connection = e.target.result;
+
+                    resolve(connection);
+                };
+
+                openRequest.onerror = e => {
+
+                    console(e.target.error);
+
+                    reject(e.target.error.name);
+                };
+            });
+        }
+
+        static _createStores(connection) {
+
+            stores.forEach(store => {
+
+                if (connection.objectStoreNames.contains(store)) connection.deleteObjectStore(store);
+
+                connection.createObjectStores(store, { autoIncrement: true });
+            });
+
+        }
     }
-
-    static getConnection() {
-
-        return new Promise((resolve, reject) => {
-
-            let openRequest = window.indexedDB.open(dbName, version);
-
-            openRequest.onupgradeneeded = e => {
-                
-                ConnectionFactory._createStores(e.target.result);
-            };
-            
-            openRequest.onsuccess = e => {
-                
-                resolve(e.target.result);
-            };
-            
-            openRequest.onerror = e => {
-                
-                console(e.target.error);
-
-                reject(e.target.error.name);
-            };
-        });
-    }
-    
-    static _createStores(connection) {
-        
-        stores.forEach(store => {
-    
-            if(connection.objectStoreNames.contains(store)) connection.deleteObjectStore(store);
-    
-            connection.createObjectStores(store, { autoIncrement: true });
-        });
-        
-    }
-}
+})()
